7f0369eac6945ad42cae57593b6905c19dbefcc7
FWXG <krel2017@gmail.com>
2022-02-05 21:54:20 +0300

    start window

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
+#include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Simulation");
 
     // run the program as long as the window is open
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
+        sf::Color window_color(27, 45, 78, 255);
+
+        while (window.isOpen())
         {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
+
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+
+            window.clear(window_color);
+
+            window.display();
+
         }
     }
 
